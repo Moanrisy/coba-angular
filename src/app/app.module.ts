d1f9cedc330f813965b1b37de3722507dbd088d1
Moanrisy <moanrisy@gmail.com>
2023-03-06 18:43:26 +0700

    Last code before 6 march 2023

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PagesComponent } from './pages/pages.component';
+import { PagesModule } from './pages/pages.module';
+import { SampleRouteModule } from './sample-route/sample-route.module';
+import { AppRoutingModule } from './app-routing.module';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    PagesModule,
+    SampleRouteModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
